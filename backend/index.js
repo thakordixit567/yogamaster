d1f39deb97389962b7092d1347b930be1c82cb77
thakordixit567 <103193972+thakordixit567@users.noreply.github.com>
2024-03-21 12:47:21 +0530

    Update Data

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,52 @@
-
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello Developers Dixit 2024')
-})
+//monoDB Connection
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@yogamaster.ueaqpqh.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // Send a ping to confirm a successful connection
+    // Create a Database
+    const database = client.db("yoga-master");
+    const userCollection = database.collection("users");
+    const classesCollection = database.collection("classes");
+    const cartCollection = database.collection("cart");
+    const paymentCollection = database.collection("payment");
+    const enrollCollection = database.collection("enroll");
+    const appliedCollection = database.collection("applied");
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello Developers Dixit 2024");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
